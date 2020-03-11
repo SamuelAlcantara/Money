e453d373f1bba4a26196bd2d335056e41eb64118
SamuelAlcantara <samuel.pereira1@outlook.com.br>
2020-03-11 09:44:39 -0300

    Remoção Import Modulo Teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { CurrencyMaskModule } from 'ng2-currency-mask';
-
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
 import { DropdownModule } from 'primeng/dropdown';
@@ -15,7 +13,6 @@ import { PaginatorModule } from 'primeng/paginator';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
-
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
@@ -24,7 +21,6 @@ import { PesquisaPessoaComponent } from './pesquisa-pessoa/pesquisa-pessoa.compo
 import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 import { TabelaPessoaComponent } from './tabela-pessoa/tabela-pessoa.component';
 import { TableComponent } from './table/table.component';
-import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +30,6 @@ import { TesteComponent } from './teste/teste.component';
     NavbarComponent,
     PesquisaPessoaComponent,
     TabelaPessoaComponent,
-    TesteComponent,
     LancamentoCadastroComponent,
     PessoaCadastroComponent
   ],
